bc61b7eca06a6281c171c626b593f71d55e13636
Jasson Bladimir Navarro Giron <jassonnavarro460@gmail.com>
2025-08-18 21:59:01 -0600

    avanze en el controlador de Servicio

--- src/main/java/org/esfe/controladores/ServicioController.java
+++ src/main/java/org/esfe/controladores/ServicioController.java
@@ -1,10 +1,40 @@
 package org.esfe.controladores;
 
+import org.springframework.ui.Model;
+import org.esfe.modelos.Servicio;
+import org.esfe.servicios.interfaces.IServicioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/servicios")
 public class ServicioController {
 
+    @Autowired
+    private IServicioService servicioService;
+
+    @GetMapping
+    public String index(Model model, @RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size,
+                        @RequestParam(value = "nombre", required = false) String nombre,
+                        @RequestParam(value = "precio", required = false) Double precio) {
+
+        int currentPage = page.orElse(1) - 1;
+        int pageSize = size.orElse(5);
+        Pageable pageable = (Pageable) PageRequest.of(currentPage, pageSize);
+
+        Page<Servicio> servicios = servicioService.buscarPorNombreyPrecio(nombre,  precio, pageable);
+        model.addAttribute("servicios", servicios);
+
+        return "servicios/index";
+
+    }
 }
