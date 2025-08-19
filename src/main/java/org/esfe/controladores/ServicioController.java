d78ab3f7b60f5ff2abacc1adb8e1949c5cf5031b
Jasson Bladimir Navarro Giron <jassonnavarro460@gmail.com>
2025-08-18 20:50:05 -0600

    controlador de Servicio inicado

---
+++ src/main/java/org/esfe/controladores/ServicioController.java
@@ -0,0 +1,10 @@
+package org.esfe.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/servicios")
+public class ServicioController {
+
+}
