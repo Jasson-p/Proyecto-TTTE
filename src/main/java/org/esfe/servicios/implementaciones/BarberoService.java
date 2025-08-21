96166ff802a8557bab1aaf0cc86a20910dee742f
Cristian Daniel Colocho López <colochocristian@outlook.com>
2025-08-20 22:10:11 -0600

    Creando vista crear, modificando codigo

--- src/main/java/org/esfe/servicios/implementaciones/BarberoService.java
+++ src/main/java/org/esfe/servicios/implementaciones/BarberoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -37,11 +38,13 @@ public class BarberoService implements IBarberoService {
     }
 
     @Override
+    @Transactional // Para operaciones de escritura
     public Barbero crearOEditar(Barbero barbero) {
         return barberoRepository.save(barbero);
     }
 
     @Override
+    @Transactional // Para operaciones de escritura
     public void eliminarPorId(Integer id) {
         barberoRepository.deleteById(id);
     }
