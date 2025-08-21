5797385228df870827bc39bdedac586d5d8f9100
Jasson Bladimir Navarro Giron <jassonnavarro460@gmail.com>
2025-08-20 21:14:08 -0600

    metodo de crear nuevo servicio agregado al controlador de Servicio

--- src/main/java/org/esfe/controladores/ServicioController.java
+++ src/main/java/org/esfe/controladores/ServicioController.java
@@ -8,9 +8,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 import java.util.List;
@@ -49,9 +52,23 @@ public class ServicioController {
         }
         return "servicio/index";
 
+    }
 
+    @GetMapping("/create")
+    public String create(Servicio servicio){
+        return "servicio/create";
+    }
 
+    @PostMapping("/save")
+    public String save(Servicio servicio, BindingResult result, Model model, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            model.addAttribute(servicio);
+            attributes.addFlashAttribute("error", "No se pudo guardar debido a un error.");
+            return "servicio/create";
+        }
 
-
+        servicioService.crearOEditar(servicio);
+        attributes.addFlashAttribute("msg", "Servicio creado correctamente");
+        return "redirect:/servicios";
     }
 }
