b9131debff92b530977c6cb4238e2eb3483a925f
Cristian Daniel Colocho López <colochocristian@outlook.com>
2025-08-22 11:33:16 -0600

    Creacion de vista index y programacion de controlador administrador

--- src/main/java/org/esfe/controladores/AdministradorController.java
+++ src/main/java/org/esfe/controladores/AdministradorController.java
@@ -1,4 +1,50 @@
 package org.esfe.controladores;
 
+
+import org.esfe.modelos.Administrador;
+import org.esfe.servicios.interfaces.IAdministradorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Controller
+@RequestMapping("/administrador")
 public class AdministradorController {
+
+    @Autowired
+    private IAdministradorService administradorService;
+
+    @GetMapping
+    public String index(Model model, @RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size, @RequestParam("nombre") Optional<String> nombre) {
+        int currentPage = page.orElse(1) - 1; // si no está seteado se asigna 0
+        int pageSize = size.orElse(5); // tamaño de la página, se asigna 5
+        Pageable pageable = PageRequest.of(currentPage, pageSize);
+        String nombreSearch = nombre.orElse("");
+        Page<Administrador> administradores = administradorService.buscarPorNombre(nombreSearch, pageable);
+        model.addAttribute("administradores", administradores);
+        // Para mantener los valores de búsqueda en el formulario HTML cuando la página se recarga
+        model.addAttribute("nombreSearch", nombreSearch);
+
+        int totalPages = administradores.getTotalPages();
+        if (totalPages > 0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+
+
+        }
+        return "administrador/index";
+    }
 }
